6b762206f04df4fa14e3a22c531f2a60cce588eb
Franklin Azodo <frankphire257@gmail.com>
2023-02-17 03:16:56 +0100

    Array work

---
+++ app.js
@@ -0,0 +1,35 @@
+//  Answer 1
+
+let myArray = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  [10, 11, 12],
+  [13, 4, 112],
+  [136, 14],];
+   
+  console.log(myArray [3] [0]);
+  console.log(myArray [2] [1]);
+  console.log(myArray [4] [0]);
+  console.log(myArray [5] [0]);
+  console.log(myArray [1] [0]);
+
+
+//  Answer 2
+var sentences = ["javascript", "web", "Capacity Bay"]
+let skill = ("javascript");
+let place= ("web");
+let company = ("Capacity Bay");
+document.getElementById("green").innerHTML = ("I am a " + place + "  developer at  "  +  company+ "and i love  "  + skill);
+
+//  Answer 3
+   let world = ("Generalization");
+   let result = world.slice(3);
+   document.getElementById("cute").innerHTML = result;
+
+//  Answer 4
+    let ownArray=[
+      ["pea", "banana"] ,
+      ["orange", "apple", "guava", "lemon"],
+      ["Avocado", "mango"],
+    ];
